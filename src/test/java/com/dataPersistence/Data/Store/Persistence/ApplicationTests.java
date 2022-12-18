c6e42c883a36fb09affbf00c15902ead74eea7d5
sol namgung <nandaras0103@gmail.com>
2022-12-18 12:56:41 +0900

    first commit for the class data persistence of Udacity

---
+++ src/test/java/com/dataPersistence/Data/Store/Persistence/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataPersistence.Data.Store.Persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
